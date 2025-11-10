caf7003b4239e37ee1daf366a785d68a84f11328
Prokudin Dmytro <dmitriy.prokudin.vl@gmail.com>
2025-11-10 01:46:22 +0100

    add resume or job description upload

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
-from typing import Union
-
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from uuid import uuid4, UUID
 
 app = FastAPI()
 
 
+class PromptData(BaseModel):
+    """ Model for promt.
+
+    Keyword arguments: \n
+    prompt -- base of the promt,\n
+    info -- resume or job decsription text,\n
+    is_job_info -- bolean value wether it is resume or job decsription.
+    """
+
+    id: UUID = Field(default_factory=uuid4)
+    prompt: str | None = None
+    info: str
+    is_job_info: bool
+
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"The context": "I am the cover letter creation app"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/dispatch/")
+def send_promt_data(prompt: PromptData):
+    return {"prompt": prompt}
