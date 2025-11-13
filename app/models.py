d89ad254c3cfcd94c12b567019f8cd1f229b8f90
Prokudin Dmytro <dmitriy.prokudin.vl@gmail.com>
2025-11-13 17:12:25 +0100

    feat: add handling info extraction from PDF and DOCX files

--- app/models.py
+++ app/models.py
@@ -1,17 +1,30 @@
+from fastapi import Form
 from pydantic import BaseModel, Field
 from uuid import uuid4, UUID
 
 
 class PromptData(BaseModel):
-    """ Model for promt.
+    """
+    Model for promt.
 
-    Keyword parameters: \n
-    prompt -- base of the promt,\n
-    info -- resume or job decsription text,\n
-    is_job_info -- bolean value wether it is resume or job decsription.
+    Attributes:
+        id: unique identifier.
+        prompt: base of the promt.
+        info: resume or job decsription text.
+        is_job_info: bolean value wether it is resume or job decsription.
     """
 
     id: UUID = Field(default_factory=uuid4)
     prompt: str | None = None
     info: str
     is_job_info: bool
+
+    @classmethod
+    def as_form(
+        cls,
+        prompt: str | None = Form(None),
+        info: str | None = Form(None),
+        is_job_info: bool = Form(...)
+    ):
+        """Allows FastAPI to build the model from form data."""
+        return cls(prompt=prompt, info=info, is_job_info=is_job_info)
--- app/utils.py
+++ app/utils.py
@@ -1,3 +1,8 @@
+import os
+import pymupdf
+from io import BytesIO
+from docx import Document
+from fastapi import HTTPException
 from .models import PromptData
 
 
@@ -15,3 +20,58 @@ def get_structured_prompt(prompt: PromptData) -> str:
     final_prompt: str = default_prompt if prompt.prompt is None else prompt.prompt
 
     return f"${final_prompt} ${choice_info} info:\n ${info}"
+
+
+def get_extracted_text(bytes_data: bytes, content_type: str, filename: str) -> str:
+    """
+    Extract text from a file.
+
+    Parameters:
+        bytes_data: raw bytes of the uploaded file.
+        content_type: FastAPI-provided MIME type.
+        filename: name of the file to detect extension.
+
+    Returns:
+        Extracted text as a string.
+
+    Raises:
+        HTTPException: If the file is not a valid PDF or contains no extractable text.
+    """
+
+    _, ext = os.path.splitext(filename.lower())
+    ext: str = ext.strip()
+
+    is_pdf: bool = (ext == ".pdf" or content_type == "application/pdf")
+
+    is_docx: bool = (
+        ext == ".docx"
+        or content_type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+    )
+
+    if is_pdf:
+        try:
+            doc = pymupdf.open(stream=bytes_data, filetype="pdf")
+        except Exception:
+            raise HTTPException(400, "File is not a valid PDF.")
+        else:
+            extracted = "\n".join(page.get_text("text") for page in doc).strip()
+
+        if not extracted:
+            raise HTTPException(400, "PDF contains no extractable text.")
+
+        return extracted
+    elif is_docx:
+        try:
+            buffer = BytesIO(bytes_data)
+            doc = Document(buffer)
+        except Exception:
+            raise HTTPException(400, "Invalid or corrupted DOCX file.")
+        else:
+            extracted = "\n".join(p.text for p in doc.paragraphs).strip()
+
+        if not extracted:
+            raise HTTPException(400, "DOCX file contains no readable text.")
+
+        return extracted
+    else:
+        raise HTTPException(400, "File should be only of format PDF or DOCX.")
